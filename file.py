4648286199fef4027fbcb7cb42ce18fa19b59e31
Jeff <j3ffh95@gmail.com>
2024-12-05 23:32:53 -0500

    Another method to create a file

--- file.py
+++ file.py
@@ -50,3 +50,8 @@ f = open("names.txt")
 # Opens a file for writing, creates the file if it does not exist
 f = open("name_list.txt", "w")
 f.close()
+
+# Creates the specified file, but returns an error if the file exists
+if not os.path.exists("jeff.txt"):
+    f = open("jeff.txt", "x")
+    f.close()
